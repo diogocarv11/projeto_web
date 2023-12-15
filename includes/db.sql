aa7a1bb0f6bc2de828acf5569b927ca9b96b7403
Andre12342 <126092255+Andre12342@users.noreply.github.com>
2023-12-15 09:57:00 +0000

    Add files via upload
    Criação da base de dados

---
+++ includes/db.sql
@@ -0,0 +1,41 @@
+-- Criação da Base de Dados
+CREATE DATABASE IF NOT EXISTS agenda_eventos;
+USE agenda_eventos;
+
+-- Tabela de Evento
+CREATE TABLE IF NOT EXISTS evento (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(255) NOT NULL,
+    data DATE NOT NULL,
+    horario TIME NOT NULL,
+    local VARCHAR(255) NOT NULL,
+    descricao TEXT
+);
+
+-- Tabela de Users
+CREATE TABLE IF NOT EXISTS user (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    senha VARCHAR(255) NOT NULL
+);
+
+-- Tabela de Participantes
+CREATE TABLE IF NOT EXISTS participante (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    user_id INT,
+    evento_id INT,
+    FOREIGN KEY (user_id) REFERENCES user(id),
+    FOREIGN KEY (evento_id) REFERENCES eventos(id)
+);
+
+-- Tabela de Comentários
+CREATE TABLE IF NOT EXISTS comentario (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    user_id INT,
+    evento_id INT,
+    comentario TEXT,
+    data_comentario DATETIME,
+    FOREIGN KEY (user_id) REFERENCES user(id),
+    FOREIGN KEY (evento_id) REFERENCES eventos(id)
+);
