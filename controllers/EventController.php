17364edf7b1a990720205b44b4625ca55a38c6f9
diogocarv11 <diogomocarvalho11@gmail.com>
2023-12-15 11:37:25 +0000

    criar functions para listar
    criar functions para listar comentarios e eventos

--- controllers/EventController.php
+++ controllers/EventController.php
@@ -1,1 +1,30 @@
+<?php
+class EventController
+{
+    public function listarEventos($pdo)
+    {
+        $query = "SELECT DISTINCT * FROM evento";
+        $stmt = $pdo->query($query);
+        $eventos = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
+        foreach ($eventos as &$evento) {
+            $evento['comentarios'] = $this->listarComentarios($pdo, $evento['id']);
+        }
+
+        include('../views/list_eventos.php');
+    }
+
+    public function listarComentarios($pdo, $eventoId)
+    {
+        $query = "SELECT c.*, u.nome AS user_nome FROM comentario c
+                  RIGHT JOIN user u ON c.user_id = u.id
+                  WHERE evento_id = :eventoId";
+        $stmt = $pdo->prepare($query);
+        $stmt->bindParam(':eventoId', $eventoId, PDO::PARAM_INT);
+        $stmt->execute();
+
+        $comentarios = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+        return $comentarios;
+    }
+}
