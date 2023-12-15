e787a433e9cc7fbe9b21a7824d445111d865b0bb
diogocarv11 <diogomocarvalho11@gmail.com>
2023-12-15 19:02:52 +0000

    listar eventos e respetivos comentarios

--- views/list_eventos.php
+++ views/list_eventos.php
@@ -1,5 +1,3 @@
-<!-- list_eventos.php -->
-
 <!DOCTYPE html>
 <html lang="pt-pt">
 
@@ -11,33 +9,35 @@
 
 <body>
     <h1>Lista de Eventos</h1>
+    <?php if (!empty($eventos) && is_array($eventos)) : ?>
+        <?php foreach ($eventos as $evento) : ?>
+            <div>
+                <h2><?php echo $evento['nome']; ?></h2>
+                <p>Data: <?php echo $evento['data']; ?></p>
+                <p>Horário: <?php echo $evento['horario']; ?></p>
+                <p>Local: <?php echo $evento['local']; ?></p>
+                <p>Descrição: <?php echo $evento['descricao']; ?></p>
+                <h3>Comentários:</h3>
 
-    <?php foreach ($eventos as $evento) : ?>
-        <div>
-            <h2><?php echo $evento['nome']; ?></h2>
-            <p>Data: <?php echo $evento['data']; ?></p>
-            <p>Horário: <?php echo $evento['horario']; ?></p>
-            <p>Local: <?php echo $evento['local']; ?></p>
-            <p>Descrição: <?php echo $evento['descricao']; ?></p>
-            <h3>Comentários:</h3>
+                <?php if (!empty($evento['comentarios'])) : ?>
+                    <ul>
+                        <?php foreach ($evento['comentarios'] as $comentario) : ?>
+                            <li>
+                                <p><?php echo $comentario['comentario']; ?></p>
+                                <p>Data: <?php echo $comentario['data_comentario']; ?></p>
+                                <p>Autor: <?php echo $comentario['user_nome']; ?></p>
+                            </li>
+                        <?php endforeach; ?>
+                    </ul>
+                <?php else : ?>
+                    <p>Sem comentários</p>
+                <?php endif; ?>
 
-            <?php if (!empty($evento['comentarios'])) : ?>
-                <ul>
-                    <?php foreach ($evento['comentarios'] as $comentario) : ?>
-                        <li>
-                            <p><?php echo $comentario['comentario']; ?></p>
-                            <p>Data: <?php echo $comentario['data_comentario']; ?></p>
-                            <p>Autor: <?php echo $comentario['user_nome']; ?></p>
-                        </li>
-                    <?php endforeach; ?>
-                </ul>
-            <?php else : ?>
-                <p>Sem comentários</p>
-            <?php endif; ?>
-
-        </div>
-    <?php endforeach; ?>
-
+            </div>
+        <?php endforeach; ?>
+    <?php else : ?>
+        <p>Nenhum evento disponível</p>
+    <?php endif; ?>
 </body>
 
 </html>
