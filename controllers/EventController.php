a1ec037578ee8d5826fd32b4affd06a7dc7191be
diogocarv11 <diogomocarvalho11@gmail.com>
2023-12-15 19:05:37 +0000

    Update EventController.php
    include('models/EventModel.php') para simplificar código
    
    uso de $key no array

--- controllers/EventController.php
+++ controllers/EventController.php
@@ -1,30 +1,34 @@
 <?php
+include('models/EventModel.php');
+
 class EventController
 {
     public function listarEventos($pdo)
     {
-        $query = "SELECT DISTINCT * FROM evento";
-        $stmt = $pdo->query($query);
-        $eventos = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        $eventModel = new EventModel($pdo);
+        $eventos = $eventModel->listarEventos();
 
-        foreach ($eventos as &$evento) {
-            $evento['comentarios'] = $this->listarComentarios($pdo, $evento['id']);
+        foreach ($eventos as $key => $evento) {
+            $eventos[$key]['comentarios'] = $this->listarComentarios($pdo, $evento['id']);
         }
-
-        include('../views/list_eventos.php');
+        include('views/list_eventos.php');
     }
 
     public function listarComentarios($pdo, $eventoId)
     {
-        $query = "SELECT c.*, u.nome AS user_nome FROM comentario c
-                  RIGHT JOIN user u ON c.user_id = u.id
+        if (isset($_SESSION['user_id'])) {
+            $query = "SELECT c.*, u.nome AS user_nome FROM comentario c
+                  INNER JOIN user u ON c.user_id = u.id
                   WHERE evento_id = :eventoId";
-        $stmt = $pdo->prepare($query);
-        $stmt->bindParam(':eventoId', $eventoId, PDO::PARAM_INT);
-        $stmt->execute();
+            $stmt = $pdo->prepare($query);
+            $stmt->bindParam(':eventoId', $eventoId, PDO::PARAM_INT);
+            $stmt->execute();
 
-        $comentarios = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
+            $comentarios = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        } else {
+            header('Location: login.php');
+            exit();
+        }
         return $comentarios;
     }
 }
