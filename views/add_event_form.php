eece687ce3a3fbb679a726424dcfc4e483a22cc8
diogocarv11 <diogomocarvalho11@gmail.com>
2023-12-21 18:48:05 +0000

    create add_event_form.php
    Formulário HTML para adicionar eventos

---
+++ views/add_event_form.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="pt-pt">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Adicionar Evento</title>
+</head>
+
+<body>
+    <h1>Adicionar Evento</h1>
+
+    <form method="post" action="processar_adicao_evento.php">
+        <label for="nome">Nome:</label>
+        <input type="text" name="nome" required>
+
+        <label for="data">Data:</label>
+        <input type="date" name="data" required>
+
+        <label for="horario">Horário:</label>
+        <input type="time" name="horario" required>
+
+        <label for="local">Local:</label>
+        <input type="text" name="local" required>
+
+        <label for="descricao">Descrição:</label>
+        <textarea name="descricao" rows="4" required></textarea>
+
+        <button type="submit">Adicionar Evento</button>
+    </form>
+</body>
+
+</html>
