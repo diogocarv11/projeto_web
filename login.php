2c8585b5b205c7f5577b8e8ae1c3555d5c8be02e
diogocarv11 <diogomocarvalho11@gmail.com>
2024-01-09 18:45:29 +0000

    add style login.php
    add bootstrap e css link, modificar design

--- login.php
+++ login.php
@@ -31,24 +31,32 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="css/styles.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <title>Login</title>
 </head>
 
 <body>
-    <h1>Login</h1>
-    <?php if (isset($error_message)) : ?>
-        <p><?php echo $error_message; ?></p>
-    <?php endif; ?>
+    <div class="cardLogin">
+        <div class="cardLogin-body">
+            <div class="imglogin"><img src="img/loginIcon.png" width="50px" height="50px"></div><br>
+            <?php if (isset($error_message)) : ?>
+                <p><?php echo $error_message; ?></p>
+            <?php endif; ?>
 
-    <form method="post" action="login.php">
-        <label for="nome">Nome:</label>
-        <input type="text" name="nome" required>
-
-        <label for="password">Password:</label>
-        <input type="password" name="password" required>
-
-        <button type="submit">Login</button>
-    </form>
+            <form method="post" action="login.php">
+                <div class="form-group">
+                    <label for="nome"><strong>Nome</strong></label>
+                    <input type="nome" name="nome" class="form-control" id="nome" placeholder="insira o nome" required>
+                </div>
+                <div class="form-group">
+                    <label for="password" font-weight="bold"><strong>Password</strong></label>
+                    <input type="password" name="password" class="form-control" id="password" placeholder="insira a password" required>
+                </div>
+                <button type="submit" class="btn btn-primary">Login</button>
+            </form>
+        </div>
+    </div>
 </body>
 
 </html>
