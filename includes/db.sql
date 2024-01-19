0b4d8226845438e2bc3dc93e8b000d53617b8c19
diogocarv11 <diogomocarvalho11@gmail.com>
2024-01-19 07:11:49 +0000

    update db file

--- includes/db.sql
+++ includes/db.sql
@@ -1,41 +1,129 @@
--- Criação da Base de Dados
-CREATE DATABASE IF NOT EXISTS agenda_eventos;
-USE agenda_eventos;
 
--- Tabela de Evento
-CREATE TABLE IF NOT EXISTS evento (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    nome VARCHAR(255) NOT NULL,
-    data DATE NOT NULL,
-    horario TIME NOT NULL,
-    local VARCHAR(255) NOT NULL,
-    descricao TEXT
-);
 
--- Tabela de Users
-CREATE TABLE IF NOT EXISTS user (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    nome VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL,
-    password VARCHAR(255) NOT NULL
-);
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
--- Tabela de Participantes
-CREATE TABLE IF NOT EXISTS participante (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    user_id INT,
-    evento_id INT,
-    FOREIGN KEY (user_id) REFERENCES user(id),
-    FOREIGN KEY (evento_id) REFERENCES evento(id)
-);
+CREATE TABLE `comentario` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `evento_id` int(11) DEFAULT NULL,
+  `comentario` text DEFAULT NULL,
+  `data_comentario` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Tabela de Comentários
-CREATE TABLE IF NOT EXISTS comentario (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    user_id INT,
-    evento_id INT,
-    comentario TEXT,
-    data_comentario DATETIME,
-    FOREIGN KEY (user_id) REFERENCES user(id),
-    FOREIGN KEY (evento_id) REFERENCES evento(id)
-);
+--
+-- Table structure for table `evento`
+--
+
+CREATE TABLE `evento` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(255) NOT NULL,
+  `data` date NOT NULL,
+  `horario` time NOT NULL,
+  `local` varchar(255) NOT NULL,
+  `descricao` text DEFAULT NULL,
+  `lotacao` int(11) DEFAULT 0,
+  `participantes` int(11) DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
+--
+-- Table structure for table `participantes`
+--
+
+CREATE TABLE `participantes` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `evento_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `comentario`
+--
+ALTER TABLE `comentario`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `evento_id` (`evento_id`);
+
+--
+-- Indexes for table `evento`
+--
+ALTER TABLE `evento`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `participantes`
+--
+ALTER TABLE `participantes`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `evento_id` (`evento_id`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `comentario`
+--
+ALTER TABLE `comentario`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `evento`
+--
+ALTER TABLE `evento`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `participantes`
+--
+ALTER TABLE `participantes`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+
+--
+-- Constraints for table `comentario`
+--
+ALTER TABLE `comentario`
+  ADD CONSTRAINT `comentario_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`),
+  ADD CONSTRAINT `comentario_ibfk_2` FOREIGN KEY (`evento_id`) REFERENCES `evento` (`id`);
+
+--
+-- Constraints for table `participantes`
+--
+ALTER TABLE `participantes`
+  ADD CONSTRAINT `fk_participantes_evento` FOREIGN KEY (`evento_id`) REFERENCES `evento` (`id`),
+  ADD CONSTRAINT `fk_participantes_user` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`);
+COMMIT;
+
