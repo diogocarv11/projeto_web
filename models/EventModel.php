da75931865ef30e360849fa639fe76a2fbfbf8ce
diogocarv11 <diogomocarvalho11@gmail.com>
2024-01-19 07:15:45 +0000

    update
    adicionar $lotacao e funcoes atualizarParticipantes() e obterContagemParticipantes()

--- models/EventModel.php
+++ models/EventModel.php
@@ -25,17 +25,39 @@ class EventModel
         return $stmt->fetch(PDO::FETCH_ASSOC);
     }
 
-    //inserção de evento
-    public function inserirEvento($nome, $data, $horario, $local, $descricao)
+    // Inserir evento
+    public function inserirEvento($nome, $data, $horario, $local, $descricao, $lotacao)
     {
-        $query = "INSERT INTO evento (nome, data, horario, local, descricao) VALUES (:nome, :data, :horario, :local, :descricao)";
+        $query = "INSERT INTO evento (nome, data, horario, local, descricao, lotacao) 
+                  VALUES (:nome, :data, :horario, :local, :descricao, :lotacao)";
         $stmt = $this->pdo->prepare($query);
         $stmt->bindParam(':nome', $nome, PDO::PARAM_STR);
         $stmt->bindParam(':data', $data, PDO::PARAM_STR);
         $stmt->bindParam(':horario', $horario, PDO::PARAM_STR);
         $stmt->bindParam(':local', $local, PDO::PARAM_STR);
         $stmt->bindParam(':descricao', $descricao, PDO::PARAM_STR);
+        $stmt->bindParam(':lotacao', $lotacao, PDO::PARAM_INT);
         $stmt->execute();
-        return $this->pdo->lastInsertId(); // Retorna o ID do evento recém-inserido
+
+        return $this->pdo->lastInsertId();
+    }
+
+    public function atualizarParticipantes($eventoId, $novaContagem)
+    {
+        $query = "UPDATE evento SET participantes = :novaContagem WHERE id = :eventoId";
+        $stmt = $this->pdo->prepare($query);
+        $stmt->bindParam(':eventoId', $eventoId, PDO::PARAM_INT);
+        $stmt->bindParam(':novaContagem', $novaContagem, PDO::PARAM_INT);
+        $stmt->execute();
+    }
+
+    public function obterContagemParticipantes($eventoId)
+    {
+        $query = "SELECT COUNT(*) FROM participantes WHERE evento_id = :evento_id";
+        $stmt = $this->pdo->prepare($query);
+        $stmt->bindParam(':evento_id', $eventoId, PDO::PARAM_INT);
+        $stmt->execute();
+
+        return $stmt->fetchColumn();
     }
 }
